a9f92ff2f9fc3e580087ce20fc4a1da564e3ab51
zZpineapple <245759894@qq.com>
2019-07-16 09:14:46 +0800

    建立User模型

--- web/myadmin/models.py
+++ web/myadmin/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+	username = models.CharField(max_length=20, null=True)
+	password = models.CharField(max_length=77)
+	phone = models.CharField(max_length=11)
+	email = models.CharField(max_length=100)
+	age = models.IntegerField(null=True)
+	pic_url = models.CharField(max_length=100)
+	SEX_CHOICES = (
+		(0, '女'),
+		(1, '男'),
+	)
+	sex = models.CharField(max_length=1,null=True,choices=SEX_CHOICES)
+	status = models.IntegerField(default=0)
+	addtime = models.DateTimeField(auto_now_add=True)
+
+
